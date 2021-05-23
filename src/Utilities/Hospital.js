924387de9a37128cf3b86f847e98602ff9afb0c8
naqeeb2710 <naqeeb532@outlook.com>
2021-05-24 00:01:19 +0530

    added address and contact information

--- src/Utilities/Hospital.js
+++ src/Utilities/Hospital.js
@@ -4,61 +4,82 @@ const Hospital=[{
     name:"Alshifa",
     photo:"/images/alshifa.jpg",
     covid_beds:50,
-    general_beds:100
-},
+    general_beds:100,
+    address: "D-305, Abul Fazal Enclave Jamia Nagar, Okhla, New Delhi 110025 ",
+    telephone_no:" 9555163363, 9818844498"
+},   
 {
     name:"Holy Family",
     photo:"/images/holy_family.jpeg",
     covid_beds:100,
-    general_beds:200
+    general_beds:200,
+    address:"Holy Family Hospital, Okhla Road, New Delhi, 110025, Delhi, India",
+    telephone_no:"011-26845900-909, 011-26332800-809 "
+
 },
 {
     name:"Indraprasta Appolo Hospital,Jasola",
     photo:"/images/appolo.jpg",
     covid_beds:100,
-    general_beds:250
+    general_beds:250,
+    address:"Sarita Vihar, Delhi Mathura Road, New Delhi, Delhi 110076, India",
+    telephone_no:"+91-11-71791090, 1091"
 },
 {
     name:"Fortis Escort, Sukhdev Vihar",
     photo:"/images/fortis.jpeg",
     covid_beds:70,
-    general_beds:100
+    general_beds:100,
+    address:" South Delhi, off Mathura Road. Okhla Road, New Delhi - 110 025 INDIA",
+    telephone_no:"91-11-47135000 "
 },
 {
     name:" Max Super Speciality Hospital,Saket",
     photo:"/images/max.jpg",
     covid_beds:80,
-    general_beds:100
+    general_beds:100,
+    address:"No.1,2, Press Enclave Road, Mandir Marg, Saket Institutional Area, Saket, New Delhi, Delhi 110017, India",
+    telephone_no:" 011-40554055 "
 },
 {
     name:"AIIMS,Delhi",
     photo:"/images/aiims.jpg",
     covid_beds:100,
-    general_beds:200
+    general_beds:200,
+    address:" ANSARI NAGAR, NEW DELHI –110029, INDIA",
+    telephone_no:"01126594871"
 },
 {
     name:"Baba Saheb Ambedkar Hospital",
     photo:"/images/ambedkar.jpeg",
     covid_beds:100,
-    general_beds:100
+    general_beds:100,
+    address:" Mahavir MargSector 6, Rohini, North, Delhi - 110085",
+    telephone_no:"011 27055585, 011 27933256"
 },
 {
     name:"Deen Dayal Upadhyay Hospital",
     photo:"/images/deendayal.jpeg",
     covid_beds:50,
-    general_beds:100
+    general_beds:100,
+    address:"Deen Dayal Upadhyay Hospital, Hari Nagar, New Delhi -64",
+    telephone_no:"+91-11-25494402-08"
 },
 {
     name:"Govind Ballabh Pant Hospital ",
     photo:"/images/gbpant.jpeg",
     covid_beds:100,
-    general_beds:100
+    general_beds:100,
+    address:"Jawaharlal Nehru Marg, New Delhi, 110002",
+    telephone_no:"91-11, 23233001-9, 23234242,"
 },
 {
     name:"Sanjay Gandhi Memorial Hospital",
     photo:"/images/sanjaygandhi.jpeg",
     covid_beds:100,
-    general_beds:100
+    general_beds:100,
+    address:"S-Block, Near Bus Terminal, Mangolpuri, North West, Delhi - 110083",
+    telephone_no:"011 27923864, 011 27915990"
 },
 ]
 export default Hospital;
